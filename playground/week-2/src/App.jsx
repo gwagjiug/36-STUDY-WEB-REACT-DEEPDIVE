8518aec10a2ba89d6297507ea19e9ddf746ecbbf
gwagjiug <kwjo0228@naver.com>
2025-04-29 18:04:41 +0900

    feat: implement custom query hook for fetching Pokémon data in App component

--- playground/week-2/src/App.jsx
+++ playground/week-2/src/App.jsx
@@ -1,5 +1,33 @@
+import React from "react";
+import { useCustomQuery } from "./hooks/useCustomQuery";
+
 function App() {
-  return <></>;
+  const { data, isLoading, error } = useCustomQuery(
+    "https://pokeapi.co/api/v2/pokemon/ditto",
+  );
+
+  if (isLoading) {
+    return (
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+      >
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    console.log(data),
+    (
+      <div>
+        <h1>곽지욱</h1>
+      </div>
+    )
+  );
 }
 
 export default App;
